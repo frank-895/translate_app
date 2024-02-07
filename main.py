fb8165433344588b78cd8c286e4ebd6fb10a0061
Frank <frank.snelling03@gmail.com>
2024-02-07 14:36:41 +0000

    class for container set up

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ import googletrans
 from googletrans import Translator
 from tkinter import ttk, messagebox
 
-class App(tk.Tk):
+
+
+
+
+class translateApp(tk.Tk):
     def __init__(self):
         super().__init__() # call superclass method
 
         self.title("Google Translate App") # name window
         self.geometry('1000x400') # change window size
 
+    def create_label(self):
 
 
     
     
 if __name__ == "__main__":
-    app = App()
+    app = translateApp()
     app.mainloop()
