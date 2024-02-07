f0465ba7c19f8869502b05e674d84d95b0ad4c8f
Frank <frank.snelling03@gmail.com>
2024-02-07 14:26:32 +0000

    window created

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import tkinter as tk
+import googletrans
+from googletrans import Translator
+from tkinter import ttk, messagebox
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__() # call superclass method
+
+        self.title("Google Translate App") # name window
+        self.geometry('1000x400') # change window size
+
+
+
+    
+    
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
